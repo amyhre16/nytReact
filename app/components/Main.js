da785d3cb88f7669e2bae4ff3eae8d4a875a8aeb
Anthony Myhre <anthony.myhre@utexas.edu>
2017-03-18 15:27:41 -0500

    Began creating the Main component in Main.js

---
+++ app/components/Main.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var Main = React.createClass({
+    render: function () {
+        return (
+            <div className="container">
+                <div className="row">
+                    
+                    {/* end of .row*/}
+                </div>
+                {/* end of .container*/}
+            </div>
+        );
+    }
+});
