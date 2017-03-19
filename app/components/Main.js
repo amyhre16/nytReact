f44b9d2c27295db5c5fbc20906883d1a338a5d8d
Anthony Myhre <anthony.myhre@utexas.edu>
2017-03-18 21:09:45 -0500

    Updated Main.js Added jumbotron and panel to component

--- app/components/Main.js
+++ app/components/Main.js
@@ -4,10 +4,27 @@ var Main = React.createClass({
         return (
             <div className="container">
                 <div className="row">
-                    
-                    {/* end of .row*/}
+                    <div className="jumbotron">
+                        <h1>New York Times Article Scrubber</h1>
+                        <p><em>Search for and annotate articles of interest!</em></p>
+                    </div>
                 </div>
-                {/* end of .container*/}
+                <div className="row">
+                    <div className="panel panel-default">
+                        <div className="panel panel-header">
+                            <h2><strong>Search</strong></h2>
+                        </div>
+                        <div className="panel panel-body">
+                            <label for="topic">Topic</label>
+                            <input type="text" name="topic" id="topicSearch" />
+                            <label for="startYear">Start Year</label>
+                            <input type="text" name="startYear" id="startYear" />
+                            <label for="endYear">End Year</label>
+                            <input type="text" name="endYear" id="endYear" />
+                        </div>
+                        <button type="submit" className="btn btn-primary">Search</button>
+                    </div>
+                </div>
             </div>
         );
     }
